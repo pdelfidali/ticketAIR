5830eca27f827859b837d30fb2eec1aff44cc1a8
pdelfidali <56030581+pdelfidali@users.noreply.github.com>
2021-03-15 20:34:31 +0100

    Some small changes in classes. Added Database with initialization(creating tables) and 1 method

--- TicketAir.py
+++ TicketAir.py
@@ -1,3 +1,5 @@
+import sqlite3
+
 class Flight:
     def __init__(self):
         self.origin = ''
@@ -5,11 +7,14 @@ class Flight:
         self.flightNumber = ''
         self.airplane = None
 
-    def __int__(self, origin, destination, flightNumber, airplane):
+    def __init__(self, origin, destination, flightNumber, airplane):
         self.origin = origin
         self.destination = destination
         self.flightNumber = flightNumber
         self.airplane = airplane
+
+    def values(self):
+        return f'("{self.origin}", "{self.destination}", "{self.flightNumber}", "{self.airplane.tailNumber}")'
 
 
 class Plane:
@@ -17,11 +22,13 @@ class Plane:
         self.yearOfProduction = 0
         self.flightMiles = 0
         self.capacity = 0
+        self.tailNumber = ''
 
-    def __init__(self, yearOfProduction, flightMiles, capacity):
+    def __init__(self, yearOfProduction, flightMiles, capacity, tailNumber):
         self.yearOfProduction = yearOfProduction
         self.flightMiles = flightMiles
         self.capacity = capacity
+        self.tailNumber = tailNumber
 
 class Ticket:
     def __init__(self):
@@ -63,3 +70,42 @@ class Admin (Passenger):
 
         def removePlane(self, plane):
             plane.remove(self)
+
+class DataBase:
+    def __init__(self):
+        print("Init")
+
+    def create_tables():
+        con = sqlite3.connect('ticketair.db')
+        cur = con.cursor()
+        cur.execute('''CREATE TABLE flights
+                        (origin text, destination text, flightNumber text, planeTailNumber text)
+                    ''')
+        print("Flights added")
+        cur.execute('''CREATE TABLE planes
+                        (tailNumber text, year integer, flightMiles real, capacity integer)
+                    ''')
+        print("Planes added")
+        cur.execute('''CREATE TABLE users
+                        (userID text, name text, ticketsIDs text, isAdmin integer, password text, login text)
+                    ''')
+        print("Users added")
+        cur.execute('''CREATE TABLE tickets
+                        (flightNumber text, day text, price real)
+                    ''')
+        print("Tickets added")
+        con.commit()
+        con.close()
+
+    def addFlight(self, flight):
+        con = sqlite3.connect('ticketair.db')
+        cur = con.cursor()
+        cur.execute('INSERT INTO flights VALUES ' + flight.values())
+        con.commit()
+        con.close()
+
+if __name__ == '__main__':
+    boeing737 = Plane(2000, 0, 150, 'LOTPL2115')
+    KRK2WAW = Flight('Krakow', 'Warszawa', 'LOT0354', boeing737)
+    db = DataBase()
+    db.addFlight(KRK2WAW)
